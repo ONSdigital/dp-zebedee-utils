02be3094e88ed8f9aae7ee0665cfccb2cce119c1
CarlHembrough <carlhuk@gmail.com>
2020-03-13 11:58:43 +0000

    Move data.json files into collection for each visualisation updated.
    - Moving the data.json file into the collection allows it to be identified as a page changed in florence.

--- cmd/visualisations/main.go
+++ cmd/visualisations/main.go
@@ -14,55 +14,42 @@ import (
 )
 
 var snippetReplacements = map[string]string{
-	"var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;":
-	"// var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;",
+	"var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;": "// var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;",
 
-	"ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';":
-	"// ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';",
+	"ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';": "// ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';",
 
-	"var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);":
-	"// var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);",
+	"var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);": "// var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);",
 
-	"ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';":
-	"// ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';",
+	"ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';": "// ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';",
 
-	"var _gaq = _gaq || [];":
-	"// var _gaq = _gaq || [];",
+	"var _gaq = _gaq || [];": "// var _gaq = _gaq || [];",
 
-	"_gaq.push(['_setAccount', 'UA-37894017-1']);":
-	"// _gaq.push(['_setAccount', 'UA-37894017-1']);",
+	"_gaq.push(['_setAccount', 'UA-37894017-1']);": "// _gaq.push(['_setAccount', 'UA-37894017-1']);",
 
-	"_gaq.push(['_trackPageview']);":
-	"// _gaq.push(['_trackPageview']);",
+	"_gaq.push(['_trackPageview']);": "// _gaq.push(['_trackPageview']);",
 
-	"ga('create', 'GTM-MBCBVQS');":
-	"// ga('create', 'GTM-MBCBVQS');",
+	"ga('create', 'GTM-MBCBVQS');": "// ga('create', 'GTM-MBCBVQS');",
 
-	"ga('send', 'pageview');":
-	"// ga('send', 'pageview');",
+	"ga('send', 'pageview');": "// ga('send', 'pageview');",
 
-	"ga('create', 'GTM-MBCBVQS', {'name': 'section_linger' });":
-	"// ga('create', 'GTM-MBCBVQS', {'name': 'section_linger' });",
+	"ga('create', 'GTM-MBCBVQS', {'name': 'section_linger' });": "// ga('create', 'GTM-MBCBVQS', {'name': 'section_linger' });",
 
-	"ga('create', 'UA-42055132-1', {'name': 'shorthand' });":
-	"// ga('create', 'UA-42055132-1', {'name': 'shorthand' });",
+	"ga('create', 'UA-42055132-1', {'name': 'shorthand' });": "// ga('create', 'UA-42055132-1', {'name': 'shorthand' });",
 
-	"ga('create', 'UA-37894017-2', 'auto');":
-	"// ga('create', 'UA-37894017-2', 'auto');",
+	"ga('create', 'UA-37894017-2', 'auto');": "// ga('create', 'UA-37894017-2', 'auto');",
 
-	"ga('shorthand.send', 'pageview', {\n        'dimension1': 24846,\n        'dimension2': 67636,\n        'metric1':24846,\n        'metric2':67636\n      });":
-	"// ga('shorthand.send', 'pageview', {\n        // 'dimension1': 24846,\n        // 'dimension2': 67636,\n        // 'metric1':24846,\n        // 'metric2':67636\n      // });",
+	"ga('shorthand.send', 'pageview', {\n        'dimension1': 24846,\n        'dimension2': 67636,\n        'metric1':24846,\n        'metric2':67636\n      });": "// ga('shorthand.send', 'pageview', {\n        // 'dimension1': 24846,\n        // 'dimension2': 67636,\n        // 'metric1':24846,\n        // 'metric2':67636\n      // });",
 
 	// some have windows line endings
-	"ga('shorthand.send', 'pageview', {\r\n        'dimension1': 24846,\r\n        'dimension2': 67636,\r\n        'metric1':24846,\r\n        'metric2':67636\r\n      });":
-	"// ga('shorthand.send', 'pageview', {\n        // 'dimension1': 24846,\n        // 'dimension2': 67636,\n        // 'metric1':24846,\n        // 'metric2':67636\n      // });",
+	"ga('shorthand.send', 'pageview', {\r\n        'dimension1': 24846,\r\n        'dimension2': 67636,\r\n        'metric1':24846,\r\n        'metric2':67636\r\n      });": "// ga('shorthand.send', 'pageview', {\n        // 'dimension1': 24846,\n        // 'dimension2': 67636,\n        // 'metric1':24846,\n        // 'metric2':67636\n      // });",
 }
 
 type Tracker struct {
-	numOfHtmlFiles    int
-	filesFixed        []string
-	blocked           []string
-	snippetsReplaceed map[string]int
+	numOfHtmlFiles     int
+	dataJsonFilesMoved int
+	filesFixed         []string
+	blocked            []string
+	snippetsReplaceed  map[string]int
 }
 
 func main() {
@@ -118,14 +105,45 @@ func replaceCodeInVisualisations(args *config.Args, cols *collections.Collection
 		logAndExit(err)
 	}
 
+	addDataJsonFilesToCollection(t, args, col)
+
 	log.Event(nil, "Finished", log.Data{
 		"numOfHtmlFiles":                t.numOfHtmlFiles,
 		"snippetsReplaced":              t.snippetsReplaceed,
 		"filesFixed":                    t.filesFixed,
+		"dataJsonFilesMoved":            t.dataJsonFilesMoved,
 		"numOfFilesFixed":               len(t.filesFixed),
 		"numOfFilesBlockedByCollection": len(t.blocked),
 		"filesBlockedByCollection":      t.blocked,
 	})
+}
+
+func addDataJsonFilesToCollection(t *Tracker, args *config.Args, col *collections.Collection) {
+	// for each file fixed, add the data.json to the collection
+	for _, htmlFile := range t.filesFixed {
+		pathSegments := strings.Split(htmlFile, "/")
+
+		dataJsonUri := path.Join(pathSegments[0], pathSegments[1], "data.json")
+		dataJsonMasterUri := path.Join(args.GetMasterDir(), dataJsonUri)
+
+		if _, err := os.Stat(dataJsonMasterUri); os.IsNotExist(err) {
+			fmt.Println("Skipping data json not found: " + dataJsonUri)
+			continue
+		}
+
+		fmt.Println("Moving data json from : " + dataJsonMasterUri + " to: " + dataJsonUri)
+
+		b, err := ioutil.ReadFile(dataJsonMasterUri)
+		if err != nil {
+			logAndExit(err)
+		}
+
+		if err := col.AddContent(dataJsonUri, b); err != nil {
+			logAndExit(err)
+		}
+
+		t.dataJsonFilesMoved++
+	}
 }
 
 func replaceCodeInHtmlFile(path string, t *Tracker, args *config.Args, cols *collections.Collections, col *collections.Collection) error {
@@ -158,7 +176,7 @@ func replaceCodeInHtmlFile(path string, t *Tracker, args *config.Args, cols *col
 
 		if strings.Contains(fileContents, searchString) {
 
-			checkIfFileAlreadyExistsInACollection(existingCollectionsChecked, uri, cols, t)
+			checkIfFileAlreadyExistsInACollection(&existingCollectionsChecked, uri, cols, t)
 
 			t.snippetsReplaceed[k] = t.snippetsReplaceed[k] + 1
 			fileUpdated = true
@@ -176,18 +194,17 @@ func replaceCodeInHtmlFile(path string, t *Tracker, args *config.Args, cols *col
 	return nil
 }
 
-func checkIfFileAlreadyExistsInACollection(existingCollectionsChecked bool, uri string, cols *collections.Collections, t *Tracker) string {
+func checkIfFileAlreadyExistsInACollection(existingCollectionsChecked *bool, uri string, cols *collections.Collections, t *Tracker) {
 
-	if !existingCollectionsChecked {
+	if !*existingCollectionsChecked {
 		for _, c := range cols.Collections {
 			if c.Contains(uri) {
 				t.blocked = append(t.blocked, uri)
 				break
 			}
 		}
-		existingCollectionsChecked = true
+		*existingCollectionsChecked = true
 	}
-	return uri
 }
 
 func logAndExit(err error) {
